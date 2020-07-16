1935b53fae53ce0554a71c0a9810c79ff18b67a5
Patrycja Cader <patcad@o2.pl>
2020-07-16 19:56:50 +0200

    initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello there!");
+    }
+}
