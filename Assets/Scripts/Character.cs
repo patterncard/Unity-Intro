d3e46bdf940746d8f8de5f891159c6026cd1e397
Patrycja Cader <patcad@o2.pl>
2020-07-16 20:15:27 +0200

    variables, operators

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,6 +7,22 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello there!");
+        string characterName = "Austin";
+        int characterLevel = 5;
+
+        int experience = characterLevel * 5;
+        float itemDurability = 1.567f;
+
+        experience = (int)itemDurability;
+        itemDurability = experience;
+
+        characterName = itemDurability.ToString();
+
+        bool equippable = false;
+
+        if (itemDurability > 1f)
+        {
+            experience = (int)(itemDurability * 1.5f) / characterLevel;
+        }
     }
 }
